a6cadfcacb0b4906fe50b7ab722e12b7a96579dd
hello-aaditya <aadityakumar.prof@gmail.com>
2026-05-25 08:30:25 +0530

    Add Post and Tag entities for many-to-many mapping

---
+++ springboot-hibernate-many-to-many-mapping/src/main/java/com/mydomain/springweb/manytomanymapping/entity/Tag.java
@@ -0,0 +1,72 @@
+package com.mydomain.springweb.manytomanymapping.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tags")
+public class Tag {
+
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "tags")
+	private Set<Post> posts = new HashSet<>();
+	
+	
+	public Tag() {
+
+	}
+
+
+	public Tag(String name) {
+		super();
+		this.name = name;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Set<Post> getPosts() {
+		return posts;
+	}
+
+
+	public void setPosts(Set<Post> posts) {
+		this.posts = posts;
+	}
+	
+	
+}
