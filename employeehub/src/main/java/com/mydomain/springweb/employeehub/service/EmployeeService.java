04098c1cf0550dc7bbf1314231424400c9cb660f
hello-aaditya <aadityakumar.prof@gmail.com>
2026-05-26 08:50:46 +0530

    Implement employee retrieval by ID in service layer

--- employeehub/src/main/java/com/mydomain/springweb/employeehub/service/EmployeeService.java
+++ employeehub/src/main/java/com/mydomain/springweb/employeehub/service/EmployeeService.java
@@ -8,4 +8,5 @@ import com.mydomain.springweb.employeehub.entity.Employee;
 public interface EmployeeService {
 	List<Employee> getAllEmployees();
 	void saveEmployee(Employee employee);
+	Employee getEmployeeById(Long id);
 }
--- employeehub/src/main/java/com/mydomain/springweb/employeehub/service/EmployeeServiceImpl.java
+++ employeehub/src/main/java/com/mydomain/springweb/employeehub/service/EmployeeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.mydomain.springweb.employeehub.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -24,6 +25,19 @@ public class EmployeeServiceImpl implements EmployeeService{
 	@Override
 	public void saveEmployee(Employee employee) {
 		employeeRepository.save(employee);
+	}
+	
+	@Override
+	public Employee getEmployeeById(Long id) {
+		Optional<Employee> optional = employeeRepository.findById(id);
+		Employee employee = null;
+		
+		if (optional.isPresent()) {
+			employee = optional.get();
+		} else {
+			throw new RuntimeException("Employee not found for ID: " + id);
+		}
+		return employee;
 	}
 	
 }
